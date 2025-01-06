9ac91ce04ab9ad71bb05acf53ce117a346a88a6e
Ashish Uniyal <86059184+bertoxious@users.noreply.github.com>
2025-01-06 09:23:52 +0530

    added uaauthorized in user controller

--- User-module/src/main/java/gov/saanjh/user/controller/UserController.java
+++ User-module/src/main/java/gov/saanjh/user/controller/UserController.java
@@ -1,11 +1,20 @@
 package gov.saanjh.user.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static gov.saanjh.user.domain.UserConstants.USER;
+
 @RestController
 @CrossOrigin
-@RequestMapping("")
+@RequestMapping(USER)
 public class UserController {
+
+    @GetMapping("/authorized")
+    public ResponseEntity<String> test(){
+        return ResponseEntity.ok().body("Authorized Success!");
+    }
 }
