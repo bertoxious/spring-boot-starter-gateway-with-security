276ebb240f51ab0db03350d445bac175dd8b2031
Ashish Uniyal <86059184+bertoxious@users.noreply.github.com>
2025-01-06 09:18:35 +0530

    removed extra modules

--- Cpsc/src/main/java/gov/saanjh/cpsc/portal/repository/character/CharacterPrintRepository.java
+++
@@ -1,9 +0,0 @@
-package gov.saanjh.cpsc.portal.repository.character;
-
-import gov.saanjh.cpsc.portal.domain.character.CharacterPrint;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CharacterPrintRepository extends JpaRepository<CharacterPrint, Long> {
-}
--- Cpsc/src/main/java/gov/saanjh/cpsc/portal/repository/tenant/TenantEntryRepository.java
+++
@@ -1,9 +0,0 @@
-package gov.saanjh.cpsc.portal.repository.tenant;
-
-import gov.saanjh.cpsc.portal.domain.tenant.TenantEntry;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TenantEntryRepository extends JpaRepository<TenantEntry, Long> {
-}
